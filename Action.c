959fc612c615c28b9f3973e579bf23300fe4a328
priya142 <priyapatel42.pv@gmail.com>
2021-10-01 22:10:46 +1000

    modified action.c file

--- Action.c
+++ Action.c
@@ -1,4 +1,17 @@
 Action()
 {
+	
+
+	web_custom_request("web_custom_request",
+		"URL=http;//www.blazedemo.com",
+		"Method=GET",
+		"TargetFrame=",
+		"Resource=0",
+		"Referer=",
+		"Body=",
+		LAST);
+
+	
+	
 	return 0;
 }
